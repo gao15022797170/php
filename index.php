ef18bea9f290a70dbeccfa3e646fe35e9c9f31e7
gao15022797170 <1339590125@qq.com>
2016-09-19 16:49:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-echo 'this is test ';
-
+header("content-type:text/html;charset=utf-8");
+error_reporting(E_ALL);
+//php基础学习  echo输出
+echo 'this is test '."<br />";
+echo "这是一个测试程序谢谢大概观看";
 ?>
